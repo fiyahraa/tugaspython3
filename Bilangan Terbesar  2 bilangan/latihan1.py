695dfee634a3ca38420af56bb0958dd9e657a18d
fiyahraa <luthfiyahza19@gmail.com>
2021-11-17 22:22:09 +0700

    menambahkan file program

---
+++ Bilangan Terbesar  2 bilangan/latihan1.py
@@ -0,0 +1,8 @@
+a = float (input("masukan angka pertama ="))
+b = float (input ("masukan angka kedua ="))
+
+if a > b :
+    print ("bilangan yang lebih besar adalah =",a)
+
+if a < b :
+    print ("bilangan yang lebih besar adalah =",b)
---
+++ Bilangan acak/latihan4.py
@@ -0,0 +1,9 @@
+#angka = range (0,5)
+
+n = float (input("masukan jumblah N ="))
+
+while n < 0.4 :
+    n +=0.1 
+    print (n)
+
+
